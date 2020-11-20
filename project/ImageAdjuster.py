87d5195abc7bec6e051239890027c2abae0f8edd
Jason <jasonclark602@yahoo.com>
2020-11-20 14:52:57 -0500

    Big Commit

--- project/ImageAdjuster.py
+++ project/ImageAdjuster.py
@@ -41,22 +41,7 @@ def _entropy(probabilities):
     return num
 
 
-def _difference_transormation(img):
-    img_copy = img.convert("L")
-
-    for x in range(img_copy.size[0]):
-        for y in range(img_copy.size[1]):
-            if x == 0:
-                continue
-            else:
-                num1 = img_copy.getpixel((x, y))
-                num2 = img_copy.getpixel((x - 1, y))
-                result = (num1 - num2) % 256
-                img.putpixel((x, y), result)
-    return img
-
-
-def calculations(request):
+def calculations():
     path = os.path.abspath('.') + '\media\images\project_image.jpg'
 
     path = path.replace('\\', '/')
@@ -66,7 +51,7 @@ def calculations(request):
     entropy2 = 0
 
     gray_img = img.convert("L")
-    gray_img.save("./media/images/gray_project_image.jpg")
+    
 
     total_pixels = gray_img.size[0] * gray_img.size[1]
 
@@ -80,16 +65,6 @@ def calculations(request):
     # Calculate the Entropy
     entropy = _entropy(probabilities)
 
-    difference_img = _difference_transormation(gray_img)
-    difference_img.save("./media/images/difference_image.jpg")
 
-    probabilities2 = _histogram(difference_img)
+    return entropy
 
-    # Adjust array to contain P(r) values by dividing each element by total_pixels
-    for x in range(len(probabilities2)):
-        probabilities2[x] = probabilities2[x] / total_pixels
-
-    # Calculate the Entropy
-    entropy2 = _entropy(probabilities2)
-
-    return entropy, entropy2
--- project/views.py
+++ project/views.py
@@ -1,33 +1,102 @@
 from django.shortcuts import render, redirect
 
 from .forms import *
+import numpy as np
+import os
+from PIL import Image as Img
+import math
+from .models import Adjustments as Adj, Image
+
 
 COLORS = ('red', 'green', 'blue', 'rgb')
+def _histogram(img):
+    histogram = np.zeros(256)
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            color = img.getpixel((i, j))
+            num = color
+            histogram[num] += 1
 
+    return histogram
+
+
+def _entropy(probabilities):
+    num = 0
+    for x in probabilities:
+        if x == 0:
+            continue
+        num += x * math.log2(x)
+
+    num *= -1
+
+    return num
+
+
+def calculations():
+    path = os.path.abspath('.') + '\media\images\project_image.jpg'
+
+    path = path.replace('\\', '/')
+
+    img = Img.open(path)
+    entropy = 0
+    entropy2 = 0
+
+    gray_img = img.convert("L")
+    
+
+    total_pixels = gray_img.size[0] * gray_img.size[1]
+
+    # Histogram Calculation to Get probability
+    probabilities = _histogram(gray_img)
+
+    # Adjust array to contain P(r) values by dividing each element by total_pixels
+    for x in range(len(probabilities)):
+        probabilities[x] = probabilities[x] / total_pixels
+
+    # Calculate the Entropy
+    entropy = _entropy(probabilities)
+
+
+    return entropy
 
 # Create your views here.
 def index(request):
+    entropy = calculations()
     if request.method == 'POST':
         if request.FILES:
             form = ImageForm(request.POST, request.FILES)
+            
+            context = {
+            'form': form,
+            'colors': COLORS,
+            'entropy':entropy,
+            }
 
             if form.is_valid():
-                image_field = form.cleaned_data
                 form.save()
+                img_obj = form.instance
+                context['img_obj'] = img_obj
+                
+
+                
         else:
             for c in COLORS:
                 print(request.POST[c + '_slider'])
 
-        return redirect('project/index.html')
+        return render(request, 'index.html', context)
 
     else:
         form = ImageForm()
 
+    
     context = {
         'form': form,
         'image_name': 'project_image.jpg',
-        'colors': COLORS
+        'colors': COLORS,
+        'entropy': entropy,
+     
+        
     }
-
-    return render(request, 'project/index.html', context)
+    
+    return render(request, 'index.html', context)
 
